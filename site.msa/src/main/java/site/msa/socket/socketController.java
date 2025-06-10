d41d0f6c76919037bca475d7f1ab08eff110583d
mekeyace2 <mekeyace2@gmail.com>
2025-06-10 11:35:20 +0900

    2025-06-10

--- site.msa/src/main/java/site/msa/socket/socketController.java
+++ site.msa/src/main/java/site/msa/socket/socketController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -18,7 +19,9 @@ public class socketController {
 		userDTO dto;
 	
 		//ws : @SendTo, @MessageMapping 로 접속을 해야하는 어노테이션
-		@SendTo("/chat/rooms/{roomId}")
+		@CrossOrigin(origins = "*", allowedHeaders = "*")
+		@MessageMapping("/chat/rooms/1")
+		@SendTo("/chat/rooms/{roomId}")	//ws://경로 접속을 할 수 있는 웹 소켓 Controller
 		public ResponseEntity<List<websocketconfig>> roomin(@PathVariable Long roomid){
 			this.dto.setUname("에이핑크");
 			System.out.println(roomid);
